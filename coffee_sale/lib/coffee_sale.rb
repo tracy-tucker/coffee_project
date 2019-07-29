4bb471cbcb1e8c43a89c5372d1c7f796bf7a8eb8
treuther <tracyreuther@gmail.com>
2019-07-29 07:37:47 -0500

    Delete coffee_sale.rb
    Wrong repository

--- coffee_sale/lib/coffee_sale.rb
+++
@@ -1,8 +0,0 @@
-require_relative "./coffee_sale/version"
-require_relative "./coffee_sale/cli"
-require 'pry'
-
-module CoffeeSale
-  class Error < StandardError; end
-  # Your code goes here...
-end
