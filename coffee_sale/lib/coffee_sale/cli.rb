09d435cd8bc77bb86cc6aa15891f63714116d97f
Tracy Reuther <tracyreuther@gmail.com>
2019-07-30 10:26:25 +0000

    method call correction in cli.rb

--- coffee_sale/lib/coffee_sale/cli.rb
+++ coffee_sale/lib/coffee_sale/cli.rb
@@ -6,23 +6,23 @@ class CoffeeSale::CLI
 
   def call
     puts "Welcome to CoffeeIcon: Manager's Special - Enjoy limited time deals on select brands"
-    # get_coffees
-    # get_user_selection
+    get_coffees
+    get_user_selection
     # show_coffee_descr(name, orig_price, sale_price, count, strength, flavored, organic, kosher)
   end
   
   def get_coffees
-    binding.pry
     # binding.pry
     # to be scraped instead
     @coffees = ["coffee_1", "coffee_2", "coffee_3", "coffee_4", "coffee_5", "coffee_6"]
     
   end
   
-  # def get_user_selection
-  #   @coffees.each_with_index do |index, coffee|
-  #     puts "#{index + 1} #{coffee}"
-  #   end
-  # end
+  def get_user_selection
+    @coffees.each_with_index do |index, coffee|
+      binding.pry
+      puts "#{index + 1} #{coffee}"
+    end
+  end
 
 end
