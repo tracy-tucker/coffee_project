8d5163aacacb35f811294ae5deb6c44823086143
Tracy Reuther <tracyreuther@gmail.com>
2019-08-01 20:08:17 +0000

    continue or exit implemented

--- coffee_sale/lib/coffee_sale/cli.rb
+++ coffee_sale/lib/coffee_sale/cli.rb
@@ -2,10 +2,14 @@ class CoffeeSale::CLI
 
   def call
     puts "Welcome to CoffeeIcon: Specials - Enjoy limited time deals on select brands".colorize(:light_blue)
+    @input = ""
+    until @input == "exit"
     get_coffees
     list_coffees
     get_user_selection
-    # show_coffee_descr
+    next_answer
+  end
+  goodbye
   end
   
   def get_coffees
@@ -34,21 +38,15 @@ class CoffeeSale::CLI
     coffee = @coffees[chosen_coffee - 1] # -1 because an index of an array always starts at 0
     puts "More information about #{coffee.name.colorize(:light_blue)}."
     puts "Original Price: #{coffee.orig_price} - SALE PRICE: #{coffee.sale_price}"
-    puts "Another selection? (Y/N)".colorize(:green)
-    # get_answer
   end
   
-  # def valid_answer(answer)
-  #   answer =  (("Y" || "y")? list_coffees: ("N" || "n")? puts "Good-bye": puts "Try again" list_coffees)
-  # end
+  def next_answer
+    puts "Type any key for another selection, or type 'exit'.".colorize(:green)
+    @input = gets.strip
+  end
   
-  # def get_answer
-  #   answer = gets.strip
-      
-  # end
-  
-  # def valid_continue(answer)
-  #   answer =  (("Y" || "y")? list_coffees: ("N" || "n")? puts "Good-bye": puts "Try again" list_coffees)
-  # end
+  def goodbye
+    puts "Thanks for shopping!".colorize(:light_blue)
+  end
 
 end
