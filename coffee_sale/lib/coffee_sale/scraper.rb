5ec697418992aa6921851f8225695d9b0b4681a4
Tracy Reuther <tracyreuther@gmail.com>
2019-08-02 00:48:47 +0000

    reset without details added

--- coffee_sale/lib/coffee_sale/scraper.rb
+++ coffee_sale/lib/coffee_sale/scraper.rb
@@ -7,10 +7,9 @@ class CoffeeSale::Scraper
   
   coffees.each do |c| #iterate over each coffee
     name = c.css('h4 a').text  #assign "name" to each coffee
-    link = 
     orig_price = c.css('span.price.price--non-sale').text.strip #assign "orig_price" to each coffee
     sale_price = c.css('span.price.price--withoutTax').text.strip #assign "sale_price" to each coffee
-    CoffeeSale::Coffee.new(name, orig_price, sale_price)  #passing in the iterated "name", "link", orig_price", "sale_price" to the new instance of Coffee
+    CoffeeSale::Coffee.new(name, orig_price, sale_price)  #passing in the iterated "name", "orig_price", "sale_price" to the new instance of Coffee
     end
   end
 
